89662dd6c23f2f972734fd85dae9242da1982ce8
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-03-25 12:45:09 +0100

    Introducing JS to the project

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    addEventListener();
+});
+
+function addEventListener() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.adddeventListener('click', responsiveNavegation);
+}
+
+function responsiveNavegation() {
+
+}
