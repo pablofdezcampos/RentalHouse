2ce65b54167c112c58f9e5c8c04e0442b30533a3
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-03-29 18:25:17 +0200

    Adding Table Control

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,32 @@ addTemplate('header');
         <p class="alert success">Correctly advert creation</p>
     <?php endif ?>
     <a href="/admin/properties/create.php" class="button-green-inline">Create</a>
+
+    <table class="propierties">
+
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Example Title</td>
+                <td><img src="/img/3d6fe7c6b53204c88cbdb4f6bcee9718.jpg" alt="Image" class="image-table"></td>
+                <td>500000</td>
+                <td>
+                    <a href="#" class="button-red-block">Eliminate</a>
+                    <a href="#" class="button-yellow-block">Update</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
