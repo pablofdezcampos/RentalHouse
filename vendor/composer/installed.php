1fb18532514e3a752b15e31a97b6fa3e4e600e33
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-04-05 08:57:11 +0200

    Adding Composer, Autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9c53c3da6daad42304c07c4afaeeb087526a13ad',
+        'name' => 'fecapa/rental-house',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fecapa/rental-house' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9c53c3da6daad42304c07c4afaeeb087526a13ad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
